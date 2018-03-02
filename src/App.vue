65894bc934feaac23b9a88fcbe301d9e2abbab6d
Nafy Ahmed <nafyahmed@email.arizona.edu>
2018-03-02 11:07:23 -0700

    added login files

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,54 @@
 <template>
 
-  <div id="app" style="width: 1500px">
-     <div id='matrix_element' style="width: 750px; float: left">
-    <Matrix></Matrix>
-  </div>
-  <div id='text_editor' style="width: 750px; float: right">
-    <TextEditor></TextEditor>
-  </div>
+  <div id="app">
+
+    <div id="login" v-if="sign_in">
+      <div id="loginMenu" className="container">
+        <div className="row" id="loginMenu2">
+            <div className="col-md-4"></div>
+            <div className="col-md-4 login-box">
+                <div className="col-lg-12 login-key">
+                    <i className="fa fa-key"></i>
+                </div>
+                <div className="col-lg-12 login-title">
+                </div>
+                <div className="col-lg-12 login-form">
+                    <div className="col-lg-12 login-form">
+                        <input id="txtEmail" className="form-control" type="email" placeholder = "Email"></input>
+                        <input id="txtPassword" className="form-control" type="password" placeholder="Password"></input>
+                    </div>
+                    <div className="col-lg-12 loginbttm">
+                        <div className="col-lg-12 login-btm login-text">
+                        </div>
+                        <div className="col-lg-12 login-btm login-button">
+                            <button id="btnLogin" className="btn btn-outline-primary" @click="login()">Login</button>
+                            <button id="btnSignUp" className="btn btn-outline-primary" onClick={this.signup}>Sign up</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>   
+ </div>
+
+    <div id="content" v-if="logged_in">
+       <div id='matrix_element' style="width: 750px; float: left">
+      <Matrix></Matrix>
+    </div>
+    <div id='text_editor' style="width: 750px; float: right">
+      <TextEditor></TextEditor>
+    </div>
+</div>
   <hr> 
 <!--   <label>Name</label>
   <input type="text" v-model="name" />
   <button @click="submitName()">Add</button> -->
   <div>
-    <table>
-      <!-- make table format -->
+<!--     <table>
       <tr v-for="personName of names" v-bind:key="personName['.key']">
         {{personName.name}}
       </tr>
-    </table>
+    </table> -->
   </div>
   </div>
 </template>
@@ -31,6 +62,7 @@ export default {
 <script>
 import TextEditor from './components/TextEditor'
 import Matrix from './components/Matrix'
+import Login from './components/Login'
   import jQuery from 'jquery/dist/jquery.js'
   import $ from 'jquery/dist/jquery.js'
   import modal from 'bootstrap/dist/css/bootstrap.css'
@@ -40,7 +72,9 @@ import {nodesRef} from './firebase';
 export default {
   data(){
     return{
-      name: 'Paul'
+      name: 'Paul',
+      logged_in: false,
+      sign_in: true
     }
   },
   firebase: {
@@ -49,11 +83,18 @@ export default {
   methods: {
     submitName(){
       nodesRef.push({name: this.name})
+    },
+    login(){
+      /*
+       * add firebase db authentication
+       */
+      this.sign_in = false;
+      this.logged_in = true;
     }
   },
   name: 'App',
   components: {
-    TextEditor, Matrix
+    TextEditor, Matrix, Login
   },
     mounted(){
 
